36aa9bf08e63e7212a276af188d859f827245df5
HappyRehund <rayhanardian045@gmail.com>
2025-11-21 10:29:03 +0700

    add cart and cart item repository layer

---
+++ src/main/java/com/rehund/ecommerce/repository/CartRepository.java
@@ -0,0 +1,15 @@
+package com.rehund.ecommerce.repository;
+
+import com.rehund.ecommerce.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    boolean existsByUserId(Long userId);
+
+    Optional<Cart> findByUserId(Long userId);
+}
